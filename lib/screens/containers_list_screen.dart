59bd59c510f2396a85a15bda569cdd95017cd265
yasht01 <yasht.2701@gmail.com>
2022-10-16 15:46:59 +0530

    feat: add button to add new container

--- lib/screens/containers_list_screen.dart
+++ lib/screens/containers_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../models/container_data.dart';
 import 'templates/checkbox_widget.dart';
 import 'templates/dropdown_widget.dart';
 import '../cubit/custom_form_cubit.dart';
@@ -18,22 +19,54 @@ class ContainersListScreen extends StatelessWidget {
           listChanged: (state) {
             return Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                ...state.containersList.map<Widget>(
-                  (containerData) => containerData.containerType ==
-                          ContainerType.Checkbox
-                      ? Center(
-                          child: CheckboxTemplate(containerData: containerData),
-                        )
-                      : Center(
-                          child: DropdownTemplate(containerData: containerData),
-                        ),
-                )
-              ],
+              children: generateList(state.containersList),
             );
           },
         );
       },
     );
+  }
+
+  List<Widget> generateList(List<ContainerData> dataList) {
+    List<Widget> widgetList = [];
+
+    for (var i = 0; i < dataList.length - 1; i++) {
+      widgetList.add(
+        dataList[i].containerType == ContainerType.Checkbox
+            ? Center(
+                child: CheckboxTemplate(containerData: dataList[i]),
+              )
+            : Center(
+                child: DropdownTemplate(containerData: dataList[i]),
+              ),
+      );
+    }
+
+    widgetList.add(Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        dataList.last.containerType == ContainerType.Checkbox
+            ? Center(
+                child: CheckboxTemplate(containerData: dataList.last),
+              )
+            : Center(
+                child: DropdownTemplate(containerData: dataList.last),
+              ),
+        Container(
+          margin: const EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20.0),
+            color: Colors.white,
+          ),
+          child: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          ),
+        ),
+      ],
+    ));
+
+    return widgetList;
   }
 }
